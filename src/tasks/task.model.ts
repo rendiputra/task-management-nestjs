485a9685d539e6692cd024a17f14e5fe419e7cf7
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-09-08 02:08:02 +0700

    create Task model

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,12 @@
+export interface Task {
+    id: String;
+    title: String;
+    description: String;
+    status: TaskStatus;
+}
+
+export enum TaskStatus {
+    OPEN = 'OPEN',
+    IN_PROGRESS = 'IN_PROGRESS',
+    DONE = 'DONE',
+}

5a565178d7fcb8d26d24514178cf4452dc1e4bba
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-09-14 21:14:45 +0700

    Creating Feature: Deleting a task by ID

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task-.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
@@ -22,5 +22,10 @@ export class TasksController {
   @Post()
   createTaks(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
   }
 }

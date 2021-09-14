eb52553dcef4cef3b80130a5e9188951aa742561
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-09-14 20:10:13 +0700

    Creating Feature: Getting a task by ID

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task-.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
@@ -7,9 +7,16 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  // http://localhost:3000/tasks/
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  // http://localhost:3000/tasks/{:id}
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post()

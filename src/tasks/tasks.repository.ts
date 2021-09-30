1bc89a96cc73bae67ea2a0bff5b2229b2767d7f7
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-01 04:55:54 +0700

    Refactoring: Getting task with filters and searching

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from "typeorm";
+import { Brackets, EntityRepository, Repository } from "typeorm";
 import { CreateTaskDto } from "./dto/create-task-.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 import { TaskStatus } from "./task-status.enum";
@@ -7,9 +7,25 @@ import { Task } from "./task.entity";
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
 
+    // Menggunakan query builder
     async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        const { status, search } = filterDto;
         const query = this.createQueryBuilder('task');
 
+        if (status) {
+            query.andWhere('status = :status', { status });
+        }
+
+        if (search) {
+            query.andWhere(
+                new Brackets((qb) => {
+                    qb.where('title ILIKE :search OR description ILIKE :search', {
+                        search: `%${search}%`,
+                    });
+                }),
+            );
+        }
+        
         const tasks = await query.getMany();
         return tasks;
     }

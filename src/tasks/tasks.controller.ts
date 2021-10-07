30843f954e4103d0e560aaf653d9a5b19a2d7f45
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-07 13:56:56 +0700

    Authentication: Guarding the tasks Routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { throws } from 'assert';
 import { CreateTaskDto } from './dto/create-task-.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -8,6 +9,7 @@ import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { TasksController } from './tasks.controller';
 import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository])],
+  imports: [
+    TypeOrmModule.forFeature([TasksRepository]),
+    AuthModule,
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })

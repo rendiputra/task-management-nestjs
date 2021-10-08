d0a393ec0e9cc635055e1561c70dd20b69326b61
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-09 06:37:59 +0700

    Apply Logger for AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger('AuthController');
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    this.logger.verbose(`Mencoba mendaftarkan akun dengan username: ${JSON.stringify(authCredentialsDto.username)}`);
     return this.authService.signUp(authCredentialsDto);
   }
 
@@ -15,6 +17,7 @@ export class AuthController {
   signIn(
     @Body() AuthCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
+    this.logger.verbose(`Mencoba login aplikasi dengan username: ${AuthCredentialsDto.username}, password: xxxx`);
     return this.authService.signIn(AuthCredentialsDto);
   }
 }
